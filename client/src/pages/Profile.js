83e9db6d10b1e864eb95f36bf898ef258bbdcfba
Yadi <ashleyfreedenberg@gmail.com>
2019-04-29 13:08:23 -0700

    no 404 or 500 status

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -19,69 +19,69 @@ class Profile extends Component {
     });
   }
 
-  // handleFormSubmit = event => {
-  //   event.preventDefault();
-  // };
+  handleFormSubmit = event => {
+    event.preventDefault();
+  };
 
-  // handleChange = event => {
-  //   const { name, value } = event.target;
-  //   this.setState({
-  //     [name]: value
-  //   });
-  // };
+  handleChange = event => {
+    const { name, value } = event.target;
+    this.setState({
+      [name]: value
+    });
+  };
   // eslint-disable-next-line no-dupe-class-members
   render() {
     return (
-      // <div>
+      <div>
         <div className="container Profile">
           <h1>On the profile page!</h1>
           <p>Username: {this.state.username}</p>
           <p>Email: {this.state.email}</p>
           <Link to="/">Go home</Link>
         </div>
-        // <div className="container">
-        //   <h1>W-4</h1>
-        //   <form onSubmit={this.handleFormSubmit}>
-        //     <div className="form-group">
-        //       <label htmlFor="firstName">First Name:</label>
-        //       <input className="form-control"
-        //         placeholder="First name goes here..."
-        //         name="firstName"
-        //         type="text"
-        //         id="firstName"
-        //         onChange={this.handleChange} />
-        //     </div>
-        //     <div className="form-group">
-        //       <label htmlFor="lastName">Last Name:</label>
-        //       <input className="form-control"
-        //         placeholder="Last name goes here..."
-        //         name="lastName"
-        //         type="text"
-        //         id="lastName"
-        //         onChange={this.handleChange} />
-        //     </div>
-        //     <div className="form-group">
-        //       <label htmlFor="ssn">SSN:</label>
-        //       <input className="form-control"
-        //         placeholder="SSN goes here..."
-        //         name="ssn"
-        //         type="ssn"
-        //         id="ssn"
-        //         onChange={this.handleChange} />
-        //     </div>
-        //     <div className="form-group">
-        //       <label htmlFor="pwd">Address:</label>
-        //       <input className="form-control"
-        //         placeholder="Address goes here..."
-        //         name="address"
-        //         type="address"
-        //         id="address"
-        //         onChange={this.handleChange} />
-        //     </div>
-        //     <button type="submit" className="btn btn-primary">Submit</button>
-        //   </form>
-        // </div>
-    // </div >
+        <div className="container">
+          <h1>W-4</h1>
+          <form onSubmit={this.handleFormSubmit}>
+            <div className="form-group">
+              <label htmlFor="firstName">First Name:</label>
+              <input className="form-control"
+                placeholder="First name goes here..."
+                name="firstName"
+                type="text"
+                id="firstName"
+                onChange={this.handleChange} />
+            </div>
+            <div className="form-group">
+              <label htmlFor="lastName">Last Name:</label>
+              <input className="form-control"
+                placeholder="Last name goes here..."
+                name="lastName"
+                type="text"
+                id="lastName"
+                onChange={this.handleChange} />
+            </div>
+            <div className="form-group">
+              <label htmlFor="ssn">SSN:</label>
+              <input className="form-control"
+                placeholder="SSN goes here..."
+                name="ssn"
+                type="ssn"
+                id="ssn"
+                onChange={this.handleChange} />
+            </div>
+            <div className="form-group">
+              <label htmlFor="pwd">Address:</label>
+              <input className="form-control"
+                placeholder="Address goes here..."
+                name="address"
+                type="address"
+                id="address"
+                onChange={this.handleChange} />
+            </div>
+            <button type="submit" className="btn btn-primary">Submit</button>
+          </form>
+        </div>
+    </div >
     );
   }
 }
