06dd6f36ff71e4a8dc7a455b4884dae4e2b1fb97
Yadi <ashleyfreedenberg@gmail.com>
2019-05-02 11:05:33 -0700

    created W4Form/I9Form

---
+++ client/src/components/PolicyManual/PolicyManual.js
@@ -0,0 +1,12 @@
+{/* <form>
+  <label>
+    Is going:
+          <input
+      name="isGoing"
+      type="checkbox"
+      checked={this.state.isGoing}
+      onChange={this.handleInputChange} />
+  </label>
+</form> */}
+
+// https://reactjs.org/docs/forms.html
